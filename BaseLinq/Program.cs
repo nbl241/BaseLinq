f75bbe32985e50541877066ffb66d630a9378415
nbl241 <39767922+nbl241@users.noreply.github.com>
2018-11-14 12:21:28 +0100

    ajout des methodes ConvertToArray et Trier

--- BaseLinq/Program.cs
+++ BaseLinq/Program.cs
@@ -10,11 +10,36 @@ namespace BaseLinq
     {
         static void Main(string[] args)
         {
-            List<Word> word = new List<Word>();
+            Word word = new Word();
 
             string sentenceA = "The quick brown fox jumps over the lazy dog";
             string sentenceB = "Portez ce vieux whisky au juge blond qui fume";
 
+            Word[] tabA = word.ConvertToArray(sentenceA);
+            Word[] tabB = word.ConvertToArray(sentenceB);
+
+            tabA = word.Trier(tabA);
+            tabB = word.Trier(tabB);
+
+            Console.WriteLine("senteneA");
+            Console.WriteLine("----------");
+            foreach(var tab in tabA)
+            {
+                Console.WriteLine("{0} | {1}", tab.id, tab.val);
+                Console.WriteLine("----------");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("sentenceB");
+            Console.WriteLine("----------");
+
+            foreach (var tab in tabB)
+            {
+                Console.WriteLine("{0} | {1}", tab.id, tab.val);
+                Console.WriteLine("----------");
+            }
+
+            Console.ReadKey();
         }
     }
 }
--- BaseLinq/Word.cs
+++ BaseLinq/Word.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace BaseLinq
 {
-    class Word
+    public class Word
     {
         private int ID;
         private string Value;
+
+        public Word() { }
 
         public Word(int id, string value)
         {
@@ -18,16 +20,44 @@ namespace BaseLinq
         }
 
         public int id { get => ID; set => ID = id; }
+
         public string val { get => Value; set => Value = val; }
 
-        public override string ToString()
-        {
-            return string.Format("{0}; {1}", ID, Value);
-        }
+        //public override string ToString()
+        //{
+        //    return string.Format("{0}; {1}", ID, Value);
+        //}
 
         public Word[] ConvertToArray(string sentence)
         {
+            string[] tab = sentence.Split(' ');
 
+            Word[] words = new Word[tab.Length];
+
+            int i = 0;
+
+            foreach (var word in words)
+            {
+                Word w = new Word(i + 1, tab[i]);
+                words[i] = w;
+                i++;
+            }
+
+            //for (int i = 0; i <= tab.Length - 1; i++)
+            //{
+            //    Word w = new Word(i + 1, tab[i]);
+            //    words[i] = w;
+            //}
+
+            return words;
+        }
+
+        public Word[] Trier(Word[] words)
+        {
+            var tableTrier = (from Word in words
+                             orderby Word.Value ascending
+                             select Word).ToArray();
+            return tableTrier;
         }
     }
 }
